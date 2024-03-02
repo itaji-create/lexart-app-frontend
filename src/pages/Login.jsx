ee851f7678ab16b3b84ee5bf70a60ccc297f28d7
Itaji de Carvalho <itaji.1999@gmail.com>
2024-03-02 00:12:00 +0000

    adiciona bootstrap ao projeto e estiliza pagina de logiin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './css/Login.css'
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
+      email: '',
       password: '',
       error: null,
     };
@@ -15,34 +16,67 @@ class Login extends Component {
     this.setState({ [name]: value, error: null });
   };
 
-  handleLogin = () => {
-    const { username, password } = this.state;
+  handleLogin = (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
 
-    if (!username || !password) {
+    if (!email || !password) {
       this.setState({ error: 'Preencha todos os campos' });
       return;
     }
-
+    window.location.href = "/produtos"
     console.log('Autenticação bem-sucedida!');
   };
 
   render() {
-    const { username, password, error } = this.state;
+    const { email, password, error } = this.state;
 
     return (
-      <div className="login-container">
-        <h2>Login</h2>
-        <div className="login-form">
-          <label>
-            Usuário:
-            <input type="text" name="username" value={username} onChange={this.handleInputChange} />
-          </label>
-          <label>
-            Senha:
-            <input type="password" name="password" value={password} onChange={this.handleInputChange} />
-          </label>
-          {error && <p className="error-message">{error}</p>}
-          <button onClick={this.handleLogin}>Login</button>
+      <div className="container-fluid">
+        <div className='row justify-content-center align-items-center'>
+          <form className="w-50 mt-5">
+            <h2 className="mb-3 fw-bold text-light">Login</h2>
+            <div id="input-login-box">
+              <div className="form-floating">
+                <input
+                  type="text"
+                  name="email"
+                  className="form-control mb-2"
+                  id="floatingInput"
+                  value={email}
+                  onChange={ this.handleInputChange }
+                />
+                <label htmlFor="floatingInput">Endereço de E-mail</label>
+              </div>
+              <div className="form-floating">
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control"
+                  id="floatingPassword"
+                  value={password}
+                  onChange={ this.handleInputChange }
+                />
+                <label htmlFor="floatingPassword">Password</label>
+              </div>
+            </div>
+            <div className="my-3 text-center">
+              {error && <p className="error-message">{error}</p>}
+              <button className="btn-outline-custom" onClick={this.handleLogin}>Login</button>
+              <div className='fixed-bottom '>
+                <sub className="text-light">Ainda não possui uma conta?</sub>
+                <a
+                  href="/registro"
+                  style={ { color: '#f47521' } }
+                  className="btn fw-bold"
+                  type="button"
+                >
+                  CLIQUE AQUI
+                </a>
+              <p className="text-body-secondary">© 2024</p>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     );
