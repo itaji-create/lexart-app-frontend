68bae1a5c9255afe287d0988e497b3fe25562591
itaji-create <itaji.1999@gmail.com>
2024-03-05 18:35:01 -0300

    adiciona button de logout no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,18 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <div className="container d-flex m-2 justify-content-between">
+      <div className="d-flex m-2 justify-content-between">
         <h2>Lexart App</h2>
-        <a href='/product/create' className="btn btn-success">
-          <i className="bi bi-plus"></i>
-          Criar produto
-        </a>
+        <div>
+          <a href='/product/create' className="btn btn-success m-2">
+            <i className="bi bi-plus"></i>
+            Criar produto
+          </a>
+          <a href='/' className="btn btn-danger">
+            <i class="bi bi-box-arrow-right" />
+            Logout
+          </a>
+        </div>
       </div>
     );
   }
