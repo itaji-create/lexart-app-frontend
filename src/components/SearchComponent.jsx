713a3586a84067869d4f2088816aec67ad235f68
itaji-create <itaji.1999@gmail.com>
2024-03-05 19:07:17 -0300

    melhora exibição da barra de buscas

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -20,21 +20,23 @@ class SearchComponent extends Component {
 
   render() {
     return (
-      <div className="d-flex align-items-center">
-        <input
-          type="text"
-          className="form-control mr-2"
-          placeholder="Digite sua busca..."
-          value={this.state.searchTerm}
-          onChange={this.handleInputChange}
-        />
-        <button
-          type="button"
-          className="btn-outline-custom"
-          onClick={this.handleSearch}
-        >
-          Buscar
-        </button>
+      <div className="d-flex justify-content-center">
+        <div className="d-flex w-50">
+          <input
+            type="text"
+            className="form-control mr-2"
+            placeholder="Digite sua busca..."
+            value={this.state.searchTerm}
+            onChange={this.handleInputChange}
+          />
+          <button
+            type="button"
+            className="btn-outline-custom"
+            onClick={this.handleSearch}
+          >
+            Buscar
+          </button>
+        </div>
       </div>
     );
   }
