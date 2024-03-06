aa1b4410eb2ea834ced19f4e959cb4015a05eb71
itaji-create <itaji.1999@gmail.com>
2024-03-06 17:24:35 -0300

    adiciona headers as solicitações do front para ser aceitas pelo back

--- src/utils/requests.jsx
+++ src/utils/requests.jsx
@@ -6,32 +6,46 @@ const api = axios.create({
   baseURL: url,
 });
 
-console.log(url);
-
 export const requestPost = async (endpoint, body, token) => {
   const { data } = await api.post(endpoint, body, {
-    headers: { Authorization: token },
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'https://lexart-app-frontend.vercel.app',
+    },
   });
   return data;
 };
 
 export const requestGet = async (endpoint, token) => {
   const { data } = await api.get(endpoint, {
-    headers: { Authorization: token },
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'https://lexart-app-frontend.vercel.app',
+    },
   });
   return data;
 };
 
 export const requestPut = async (endpoint, body, token) => {
   const { data } = await api.put(endpoint, body, {
-    headers: { Authorization: token },
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'https://lexart-app-frontend.vercel.app',
+    },
   });
   return data;
 };
 
 export const deleteItem = async (endpoint, id, token) => {
   await api.delete(`${endpoint}/${id}`, {
-    headers: { Authorization: token },
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'https://lexart-app-frontend.vercel.app',
+    },
   });
 };
 
